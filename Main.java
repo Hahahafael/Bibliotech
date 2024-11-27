b800be5e4343681ec68a4f8813c8b643dd41a278
Hahahafael <rafaelpfn@hotmail.com>
2024-11-27 18:27:44 -0300

    main

---
+++ Main.java
@@ -0,0 +1,28 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    try {
+      Parent root = FXMLLoader.load(getClass().getResource("/view/homeScreen.fxml"));
+      primaryStage.getIcons().add(new Image("/img/icon.png"));                            
+      primaryStage.setTitle("Bibliotech"); 
+      primaryStage.setResizable(false);         
+      primaryStage.setScene(new Scene(root, 1280, 720));         
+      primaryStage.show();          
+      primaryStage.sizeToScene();  
+      primaryStage.setOnCloseRequest(e -> {System.exit(0);}); 
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
---
+++ controller/RegisterController.java
@@ -0,0 +1,44 @@
+package controller;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class RegisterController {
+
+  private Stage window;
+  private Scene scene;
+
+  /**
+   * Changes the current screen to the login screen.
+   * 
+   * @param event the triggered event, usually a click on a button.
+   * @throws IOException if an error occurs while loading the FXML file from the login screen.
+   */
+  public void goToLoginScreen(ActionEvent event) throws IOException {
+    Parent root = FXMLLoader.load(getClass().getResource("/view/login.fxml"));
+    window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    scene = new Scene(root);
+    window.setScene(scene);
+    window.show();
+  }
+
+  /**
+   * Changes the current screen to the home screen.
+   * 
+   * @param event the triggered event, usually a click on a button.
+   * @throws IOException if an error occurs while loading the FXML file from the login screen.
+   */
+  public void goToHomeScreen(ActionEvent event) throws IOException {
+    Parent root = FXMLLoader.load(getClass().getResource("/view/homeScreen.fxml"));
+    window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    scene = new Scene(root);
+    window.setScene(scene);
+    window.show();
+  }
+}
