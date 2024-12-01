4b6ca998d753c0117b7923b3debf4a6029909bd6
AlissonAnjos21 <93294707+AlissonAnjos21@users.noreply.github.com>
2024-12-01 05:21:57 -0300

    Divisao entre interfaces DAO e classes DAO, pre-implementacao da interface IAutorDAO e da classe AutorDAO

---
+++ model/dao/interfaces/IAutorDAO.java
@@ -0,0 +1,20 @@
+package model.dao.interfaces;
+
+import model.entities.Autor;
+
+public interface IAutorDAO {
+
+  String SQL_INSERT = "INSERT INTO Autor (id, nome) VALUES (?, ?)";
+  String SQL_ALTER = "UPDATE Autor SET nome = ?, WHERE id = ?";
+  String SQL_DELETE = "DELETE FROM Autor WHERE id = ?";
+  String SQL_SELECT = "SELECT * FROM Autor WHERE id = ?";
+
+  public boolean find();
+
+  public boolean insert();
+  
+  public boolean remove();
+
+  public boolean update();
+
+}
