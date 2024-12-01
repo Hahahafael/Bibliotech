21f84a656e8261b29e196d20c020c52a7003cf08
AlissonAnjos21 <93294707+AlissonAnjos21@users.noreply.github.com>
2024-12-01 05:56:09 -0300

    Interface e Classe DAO de Categoria

---
+++ model/dao/interfaces/ICategoria.java
@@ -0,0 +1,18 @@
+package model.dao.interfaces;
+
+public interface ICategoria {
+  
+  String SQL_INSERT = "INSERT INTO Autor (id, nome) VALUES (?, ?)";
+  String SQL_ALTER = "UPDATE Autor SET nome = ?, WHERE id = ?";
+  String SQL_DELETE = "DELETE FROM Autor WHERE id = ?";
+  String SQL_SELECT = "SELECT * FROM Autor WHERE id = ?";
+
+  public boolean find();
+
+  public boolean insert();
+  
+  public boolean remove();
+
+  public boolean update();
+  
+}
