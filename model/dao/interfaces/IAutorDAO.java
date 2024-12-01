12aba612b6b5681280b6cdf51f831b4d3c1408f4
AlissonAnjos21 <93294707+AlissonAnjos21@users.noreply.github.com>
2024-12-01 08:05:01 -0300

    Corrigindo codigo SQL das Strings das Interface IAutorDAO

--- model/dao/interfaces/IAutorDAO.java
+++ model/dao/interfaces/IAutorDAO.java
@@ -3,9 +3,9 @@ package model.dao.interfaces;
 public interface IAutorDAO {
 
   String SQL_INSERT = "INSERT INTO Autor (nome) VALUES (?)";
-  String SQL_ALTER = "UPDATE Autor SET nome = ?, WHERE id = ?";
-  String SQL_DELETE = "DELETE FROM Autor WHERE id = ?";
-  String SQL_SELECT = "SELECT * FROM Autor WHERE id = ?";
+  String SQL_ALTER = "UPDATE Autor SET nome = ?, WHERE id_autor = ?";
+  String SQL_DELETE = "DELETE FROM Autor WHERE id_autor = ?";
+  String SQL_SELECT = "SELECT * FROM Autor WHERE id_autor = ?";
 
   public boolean find();
 
