6abe5db0f5c767646a735ce1ca00342e8d9a24af
AlissonAnjos21 <93294707+AlissonAnjos21@users.noreply.github.com>
2024-12-03 20:13:23 -0300

    RegisterController registro

--- controller/RegisterController.java
+++ controller/RegisterController.java
@@ -3,13 +3,23 @@ package controller;
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import model.dao.classes.UsuarioDAO;
+import model.entities.Usuario;
 
 public class RegisterController {
+  @FXML TextField NameRegister;
+  @FXML TextField EmailRegister;
+  @FXML TextField PhoneRegister;
+  @FXML TextField PasswordRegister;
+  @FXML Button RegisterButton;
 
   private Stage window;
   private Scene scene;
@@ -40,5 +50,30 @@ public class RegisterController {
     scene = new Scene(root);
     window.setScene(scene);
     window.show();
+  }
+
+  public void register(ActionEvent event) throws IOException {
+    String nome = NameRegister.getText();
+    String email = EmailRegister.getText();
+    String telefone = PhoneRegister.getText();
+    String senha = PasswordRegister.getText();
+    Usuario novoUsuario = new Usuario(nome, nome, email, telefone, senha);
+    novoUsuario.setLogin(email);
+    novoUsuario.setNome(nome);
+    novoUsuario.setEmail(email);
+    novoUsuario.setTelefone(telefone);
+    novoUsuario.setSenha(senha);
+    novoUsuario.setMultaAcumulada(0); 
+    novoUsuario.setStatusDaConta("Ativa"); 
+    novoUsuario.setTipo("Aluno"); 
+
+    UsuarioDAO usuarioDAO = new UsuarioDAO(novoUsuario);
+    boolean resultado = usuarioDAO.insert();
+
+    if (resultado) {
+      System.out.println("Usuário registrado com sucesso!");
+    } else {
+      System.out.println("Erro ao registrar usuário.");
+    }
   }
 }
