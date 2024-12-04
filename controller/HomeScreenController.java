56c2d25c4e635f3fde72346b5eb02f2df331f5dc
Hahahafael <rafaelpfn@hotmail.com>
2024-12-03 22:37:47 -0300

    juntado o bd com o login e fazendo o botao login desaparecer um vez que estamos logados

--- controller/HomeScreenController.java
+++ controller/HomeScreenController.java
@@ -20,7 +20,21 @@ public class HomeScreenController {
 
   private Stage window;
   private Scene scene;
+  private boolean isLoggedIn = false;  
 
+  public void setLoggedIn(boolean loggedIn) {
+    this.isLoggedIn = loggedIn;
+    updateLoginButtonVisibility(); 
+  }
+
+  private void updateLoginButtonVisibility() {
+    if (isLoggedIn) {
+        goToLoginScreen.setVisible(false);  // Esconde o botão se estiver logado
+    } else {
+        goToLoginScreen.setVisible(true);   // Exibe o botão se não estiver logado
+    }
+  }
+ 
 
   /**
    * Changes the current screen to the login screen.
@@ -29,11 +43,11 @@ public class HomeScreenController {
    * @throws IOException if an error occurs while loading the FXML file from the login screen.
    */
   public void goToLoginScreen(ActionEvent event) throws IOException {
-    Parent root = FXMLLoader.load(getClass().getResource("/view/login.fxml"));
-    window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-    scene = new Scene(root);
-    window.setScene(scene);
-    window.show();
+      Parent root = FXMLLoader.load(getClass().getResource("/view/login.fxml"));
+      window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+      scene = new Scene(root);
+      window.setScene(scene);
+      window.show();
   }
 
   /**
@@ -43,16 +57,16 @@ public class HomeScreenController {
    * @throws IOException if an error occurs while loading the FXML file from the choose document screen.
    */
   public void goToChooseDocumentScreen(ActionEvent event) throws IOException {
-    Parent root = FXMLLoader.load(getClass().getResource("/view/chooseDocument.fxml"));
-    window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-    scene = new Scene(root);
-    window.setScene(scene);
-    window.show();
+      Parent root = FXMLLoader.load(getClass().getResource("/view/chooseDocument.fxml"));
+      window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+      scene = new Scene(root);
+      window.setScene(scene);
+      window.show();
   }
 
-  public void showHelpRectangle(ActionEvent event) throws IOException{
-    rectangleHelp.setVisible(!rectangleHelp.isVisible());
-    imageViewHelp.setVisible(!imageViewHelp.isVisible());
+  public void showHelpRectangle(ActionEvent event) throws IOException {
+      rectangleHelp.setVisible(!rectangleHelp.isVisible());
+      imageViewHelp.setVisible(!imageViewHelp.isVisible());
   }
 
 }
--- controller/LoginController.java
+++ controller/LoginController.java
@@ -3,13 +3,21 @@ package controller;
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import model.dao.classes.UsuarioDAO;
+import model.entities.Usuario;
 
 public class LoginController {
+  @FXML TextField loginTextField;
+  @FXML TextField passwordLogin;
+  @FXML Button loginButton;
   
   private Stage window;
   private Scene scene;
@@ -40,6 +48,30 @@ public class LoginController {
     scene = new Scene(root);
     window.setScene(scene);
     window.show();
+  }
+  public void login(ActionEvent event) throws IOException {
+    String login = loginTextField.getText();
+    String senha = passwordLogin.getText();
+    if (login.isEmpty() || senha.isEmpty()) {
+        System.out.println("Por favor, preencha todos os campos!");
+        return;
+    }
+    Usuario usuario = new Usuario(login, senha);
+    UsuarioDAO usuarioDAO = new UsuarioDAO(usuario);
+    boolean autenticado = usuarioDAO.findByLogin();
+    if (autenticado) {
+        System.out.println("Login realizado com sucesso!");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/homeScreen.fxml"));
+        Parent root = loader.load();
+        HomeScreenController homeScreenController = loader.getController();
+        homeScreenController.setLoggedIn(true);  
+        window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        window.setScene(scene);
+        window.show();
+    } else {
+        System.out.println("Login ou senha inválidos.");
+    }
   }
 
 }
