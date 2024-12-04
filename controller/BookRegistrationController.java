be38a06bc076b25591814360e30d6842ab60b325
Rodrigo Aguiar <rodrigofbaguiar@gmail.com>
2024-12-04 20:36:07 -0300

    add ao github o registro de monografia

--- controller/BookRegistrationController.java
+++ controller/BookRegistrationController.java
@@ -23,18 +23,30 @@ import model.entities.Documento;
 import model.entities.Livro;
 
 public class BookRegistrationController {
-  @FXML ImageView ImageViewBookRegistration;
-  @FXML Button addPhotoButtonBookResgistration;
-  @FXML TextField codeBookRegistration;
-  @FXML TextField titleBookRegistration;
-  @FXML TextField publisherBookRegistration;
-  @FXML TextField editionBookRegistration;
-  @FXML TextField pagesBookRegistration;
-  @FXML TextField languageBookRegistration;
-  @FXML TextField categoryBookRegistration;
-  @FXML TextField categoryNumberBookRegistration;
-  @FXML Button buttonBookRegistration;
-  @FXML DatePicker dateBookRegistration;
+  @FXML
+  ImageView ImageViewBookRegistration;
+  @FXML
+  Button addPhotoButtonBookResgistration;
+  @FXML
+  TextField codeBookRegistration;
+  @FXML
+  TextField titleBookRegistration;
+  @FXML
+  TextField publisherBookRegistration;
+  @FXML
+  TextField editionBookRegistration;
+  @FXML
+  TextField pagesBookRegistration;
+  @FXML
+  TextField languageBookRegistration;
+  @FXML
+  TextField categoryBookRegistration;
+  @FXML
+  TextField categoryNumberBookRegistration;
+  @FXML
+  Button buttonBookRegistration;
+  @FXML
+  DatePicker dateBookRegistration;
 
   private Stage window;
   private Scene scene;
@@ -43,7 +55,8 @@ public class BookRegistrationController {
    * returns a page based on the page the user is on
    * 
    * @param event the triggered event, usually a click on a button.
-   * @throws IOException if an error occurs while loading the FXML file from the home screen.
+   * @throws IOException if an error occurs while loading the FXML file from the
+   *                     home screen.
    */
   public void returnPreviousPage(ActionEvent event) throws IOException {
     Parent root = FXMLLoader.load(getClass().getResource("/view/chooseDocument.fxml"));
@@ -55,12 +68,15 @@ public class BookRegistrationController {
 
   /**
    * the "edite" button can chose a photo file in your PC
+   * 
    * @param event the triggered event, usually a click on a button.
-   * @throws IOException if an error occurs while loading the FXML file from the home screen.
+   * @throws IOException if an error occurs while loading the FXML file from the
+   *                     home screen.
    */
   public void selectImage(ActionEvent event) throws IOException {
     FileChooser fileChooser = new FileChooser();
-    FileChooser.ExtensionFilter imageFilter = new FileChooser.ExtensionFilter("Imagens (JPG, PNG, GIF)", "*.jpg", "*.jpeg", "*.png", "*.gif");
+    FileChooser.ExtensionFilter imageFilter = new FileChooser.ExtensionFilter("Imagens (JPG, PNG, GIF)", "*.jpg",
+        "*.jpeg", "*.png", "*.gif");
     fileChooser.getExtensionFilters().add(imageFilter);
     File selectedFile = fileChooser.showOpenDialog(null);
     if (selectedFile != null) {
@@ -70,11 +86,12 @@ public class BookRegistrationController {
   }
 
   /**
-  * Registers a book in the system.
-  * 
-  * @param event the triggered event, usually a click on a button.
-  * @throws IOException if an error occurs while loading the FXML file from the home screen.
-  */
+   * Registers a book in the system.
+   * 
+   * @param event the triggered event, usually a click on a button.
+   * @throws IOException if an error occurs while loading the FXML file from the
+   *                     home screen.
+   */
   public void registerBook(ActionEvent event) throws IOException {
     String code = codeBookRegistration.getText();
     String title = titleBookRegistration.getText();
@@ -88,7 +105,6 @@ public class BookRegistrationController {
 
     Categoria categoria = new Categoria(categoryNumber, category);
 
-
     Documento documentoLivro = new Documento(code, title, date, null, pages, language, categoria);
     DocumentoDAO documentoLivroDAO = new DocumentoDAO(documentoLivro);
     boolean resultadoDocumento = documentoLivroDAO.insert();
@@ -96,21 +112,22 @@ public class BookRegistrationController {
     Livro livro = new Livro(code, publisher, edition);
     LivroDAO livroDAO = new LivroDAO(livro);
     boolean resultado = livroDAO.insert();
-    
+
     if (resultado) {
-    boolean resultadoLivro = livroDAO.insert();
-    
-    if (resultadoDocumento && resultadoLivro) {
-      System.out.println("Livro registrado com sucesso!");
-      FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/homeScreen.fxml"));
-      Parent root = loader.load();
-      window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-      scene = new Scene(root);
-      window.setScene(scene);
-      window.show();
-    } else {
-      System.out.println("Erro ao registrar livro.");
+      boolean resultadoLivro = livroDAO.insert();
+
+      if (resultadoDocumento && resultadoLivro) {
+        System.out.println("Livro registrado com sucesso!");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/homeScreen.fxml"));
+        Parent root = loader.load();
+        window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        window.setScene(scene);
+        window.show();
+      } else {
+        System.out.println("Erro ao registrar livro.");
+      }
     }
   }
-}
 
+}
